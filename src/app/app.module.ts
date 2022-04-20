addbd42cae4e01c7d0e558c32cc64db4c9bd4fbe
hayteam5 <hayteam797@gmail.com>
2022-04-20 14:41:11 +0200

    first exercise V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CvComponent } from './cv/cv.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CvComponent
+    CvComponent,
+    JobDetailsComponent
   ],
   imports: [
     BrowserModule
